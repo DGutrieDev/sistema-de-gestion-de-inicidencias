17c7d611a1930d0cc10c30e7372d091cbfcd6db3
DGutrieDev <dennyg.dev@gmail.com>
2024-06-02 12:26:33 -0600

    Update Modules

--- backend/src/Controllers/UsuariosController.js
+++ backend/src/Controllers/UsuariosController.js
@@ -3,33 +3,25 @@ const Usuario = require('../models/Usuarios');
 async function crearUsuario(req, res) {
     try {
         const { cedula, nombre, apellidoUno, apellidoDos, contraseña, departamento, usuario_inst, puesto } = req.body;
-        let usuarioNuevo;
-        if (usuario_inst) {
-            usuarioNuevo = await Usuario.create({
-                CT_cedula: cedula,
-                CT_nombre: nombre,
-                CT_apellidoUno: apellidoUno,
-                CT_apellidoDos: apellidoDos,
-                CT_contraseña: contraseña,
-                CT_departamento: departamento,
-                CT_usuario_institucional: usuario_inst,
-                CT_puesto: puesto
-            });
-        } else {
-            usuarioNuevo = await Usuario.create({
-                CT_cedula: cedula,
-                CT_nombre: nombre,
-                CT_apellidoUno: apellidoUno,
-                CT_apellidoDos: apellidoDos,
-                CT_contraseña: contraseña,
-                CT_departamento: departamento,
-                CT_puesto: puesto
-            });
+        if (!cedula || !nombre || !apellidoUno || !apellidoDos || !contraseña || !departamento || !puesto) {
+            return res.status(400).json({ Mensaje: 'Faltan campos requeridos' });
         }
+        const usuarioData = {
+            CT_cedula: cedula,
+            CT_nombre: nombre,
+            CT_apellidoUno: apellidoUno,
+            CT_apellidoDos: apellidoDos,
+            CT_contraseña: contraseña,
+            CT_departamento: departamento,
+            CT_usuario_institucional: usuario_inst || null,
+            CT_puesto: puesto
+        };
+        const usuarioNuevo = await Usuario.create(usuarioData)
         res.status(201).json(usuarioNuevo);
     } catch (err) {
         res.status(500).json({ Mensaje: 'Error al crear el usuario', error: err.message });
     }
 }
+
 
 module.exports = { crearUsuario };
